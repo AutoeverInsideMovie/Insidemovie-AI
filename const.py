f89a425172c783dcae99214814e93c4215ee5204
Gumraze-git <galaxydh4110@gmail.com>
2025-07-11 11:08:07 +0900

    [FEAT] 감정 상태 상수 추가
    - 7가지 및 5가지 감정 상태를 나타내는 상수 LABELS_7, LABELS_5 추가

---
+++ const.py
@@ -0,0 +1,12 @@
+# 7 가지 감정 상태
+# 기쁨, 슬픔, 분노, 놀람, 혐오, 공포, 중립
+LABELS_7 = [
+    "joy", "sadness", "anger",
+    "surprise", "disgust", "fear", "neutral"
+]
+
+# 5 가지 감정 상태
+# 기쁨, 슬픔, 분노, 공포, 중립
+LABLES_5 = [
+    "joy", "sadness", "fear", "anger", "neutral"
+]
