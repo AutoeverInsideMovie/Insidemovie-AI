a6f36c15a698e0d171434922f47fc7053b0c420e
Gumraze-git <galaxydh4110@gmail.com>
2025-07-12 19:34:25 +0900

    [FEAT] 감정 상태 상수 업데이트
    - LABELS_7 및 LABELS_5의 "joy"를 "happiness"로 변경
    - LABELS_6 상수 추가

--- const.py
+++ const.py
@@ -1,12 +1,11 @@
 # 7 가지 감정 상태
 # 기쁨, 슬픔, 분노, 놀람, 혐오, 공포, 중립
 LABELS_7 = [
-    "joy", "sadness", "anger",
-    "surprise", "disgust", "fear", "neutral"
+    "happiness", "sadness", "anger", "surprise", "disgust", "fear", "neutral"
 ]
-
-# 5 가지 감정 상태
-# 기쁨, 슬픔, 분노, 공포, 중립
+LABELS_6 = [
+    "happiness", "sadness", "anger", "surprise", "fear", "neutral"
+]
 LABLES_5 = [
-    "joy", "sadness", "fear", "anger", "neutral"
+    "happiness", "sadness", "anger", "fear", "neutral"
 ]
