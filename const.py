1fd1d2c6f9e0599fb9d4fb8cbae1b6f743a22aa4
Gumraze-git <galaxydh4110@gmail.com>
2025-07-12 19:59:55 +0900

    [CHORE] LABELS_7 라벨 순서 확인 및 수정 필요

--- const.py
+++ const.py
@@ -1,5 +1,6 @@
 # 7 가지 감정 상태
 # 기쁨, 슬픔, 분노, 놀람, 혐오, 공포, 중립
+# TODO:  7개 라벨의 라벨링 순서 엉킴. 라벨 순서 확인하여 수정 필요함.
 LABELS_7 = [
     "happiness", "sadness", "anger", "surprise", "disgust", "fear", "neutral"
 ]
