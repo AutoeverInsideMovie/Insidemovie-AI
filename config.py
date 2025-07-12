b441d67dec39122193596bd0182950a734203cea
Gumraze-git <galaxydh4110@gmail.com>
2025-07-12 19:39:31 +0900

    [FIX] 모델 디렉터리 경로 수정
    - MODEL_DIR 기본값을 "models/0712_kobert_5_emotion_model"로 변경

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ class Settings(BaseSettings):
     mongodb_uri: str = "mongodb://localhost:27017"
     db_name: str = "emotion_db"
     collection_name: str = "predictions"
-    model_dir: str = os.getenv("MODEL_DIR", "0712_kobert_5_emotion_model")
+    model_dir: str = os.getenv("MODEL_DIR", "models/0712_kobert_5_emotion_model")
     title: str = "MovieMood - KoBERT Emotion API"
 
     model_config = SettingsConfigDict(env_file=".env")
